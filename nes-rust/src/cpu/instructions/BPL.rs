b9d87cf13dbf809bfc1d75174df0859a547c6706
Paul Hazen <paul-hazen@live.com>
2025-02-20 22:37:20 -0800

    Make rust suggested fixes.

--- nes-rust/src/cpu/instructions/BPL.rs
+++ nes-rust/src/cpu/instructions/BPL.rs
@@ -1,6 +1,5 @@
 use crate::cpu::status_register::StatusRegister;
 use crate::cpu::CPU;
-use crate::memory::MemoryBus;
 use crate::define_instruction;
 
 define_instruction!(BPL, |cpu: &mut CPU, memory, operand : u8| {
