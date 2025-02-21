99a384b39b0146c53f3de8bf5e2252c961e4040f
Paul Hazen <paul-hazen@live.com>
2025-02-20 22:18:58 -0800

    Applied rust fixes.

--- nes-rust/src/cpu/instructions/TYA.rs
+++ nes-rust/src/cpu/instructions/TYA.rs
@@ -1,6 +1,4 @@
 use crate::cpu::CPU;
-use crate::cpu::instruction::Instruction;
-use crate::cpu::opcode::OpCode;
 use crate::define_instruction;
 
 define_instruction!(TYA, |cpu: &mut CPU, _, _ :u8| {
