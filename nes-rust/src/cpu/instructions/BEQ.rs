cf8f7140d8c71f55a1673cf84476cb04356830c0
Paul Hazen <paul-hazen@live.com>
2025-02-21 02:22:42 -0800

    Turning in for the evening - failing a test currently regarding getting the reset vector - but I'm tired enough that it's all greek to me.

--- nes-rust/src/cpu/instructions/BEQ.rs
+++ nes-rust/src/cpu/instructions/BEQ.rs
@@ -2,8 +2,7 @@ use crate::cpu::status_register::StatusRegister;
 use crate::cpu::CPU;
 use crate::define_instruction;
 
-define_instruction!(BEQ, |cpu: &mut CPU, _memory, _:u8| {
-    if cpu.processor_status.is_set(StatusRegister::ZERO) {
-        //cpu.branch(memory, offset);
-    }
+define_instruction!(BEQ, |cpu: &mut CPU, memory, offset:u8| {
+    let is_zero = cpu.processor_status.is_set(StatusRegister::ZERO);
+    cpu.branch(memory, is_zero, offset);
 });
--- nes-rust/src/cpu/instructions/BPL.rs
+++ nes-rust/src/cpu/instructions/BPL.rs
@@ -2,9 +2,8 @@ use crate::cpu::status_register::StatusRegister;
 use crate::cpu::CPU;
 use crate::define_instruction;
 
-define_instruction!(BPL, |cpu: &mut CPU, _memory, _operand : u8| {
-    if !cpu.processor_status.is_set(StatusRegister::NEGATIVE) {
-        //cpu.branch(memory, offset);
-    }
+define_instruction!(BPL, |cpu: &mut CPU, memory, operand : u8| {
+    let not_negative = !cpu.processor_status.is_set(StatusRegister::NEGATIVE);
+    cpu.branch(memory, not_negative, operand);
 });
 
